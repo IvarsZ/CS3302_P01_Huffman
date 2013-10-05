c2b81b3c96d898fa8f98c1121c20a90c1f5b4099
iz2 <devnull@localhost>
2013-10-05 13:30:24 +0100

    Maven setup

---
+++ src/main/java/compression/App.java
@@ -0,0 +1,13 @@
+package compression;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
